a4b447e24a13494e24d796437cda955b349d50a8
Lucas <lucasborbadesouza100@gmail.com>
2023-03-20 21:28:00 -0300

    parte do front legal

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Projeto_clean_help
  site feito em html e outras coisas, testes e afins
  vou adicionar html css e js
+ ainda nao sei bem oque vou criar mas acho que vai ser um site voltado para serviços de limpeza
+ minha mae sempre quis um kkkk
---
+++ index.js
@@ -0,0 +1,10 @@
+// Código JavaScript para suavizar a rolagem ao clicar nos links do menu
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
